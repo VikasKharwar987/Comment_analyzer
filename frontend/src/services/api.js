98cd138d0271e6e03507430694a8118528c3b6a2
Vikas Kharwar <vikaskharwar987@gmail.com>
2026-04-30 22:03:55 +0530

    fixed Backend api link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://toxicity-analyzer-backend.onrender.com',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
